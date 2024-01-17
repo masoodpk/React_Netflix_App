459f7e547919bab77c0e7bef12af292d79cf0558
masood <masoodpk555@gmail.com>
2024-01-17 11:58:18 +0530

    added navbar,banner

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import Banner from "./Components/NavBar/Banner/Banner";
+
+function App(){
+  return(
+    <div className="App">
+
+      <NavBar/>
+      <Banner/>
+    </div>
+  )
+}
+export default App;
