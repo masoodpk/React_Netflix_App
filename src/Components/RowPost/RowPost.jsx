c14fa895e83b9764f9a5d0bd50205521b321c686
masood <masoodpk555@gmail.com>
2024-01-18 11:29:26 +0530

    editing in RowPost

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './RowPost.css'
+import {API_KEY} from '../../constants/constants'
+import axios from '../../axios'
+
+// import axios
 
 function RowPost() {
+  const [movies, setMovies] = useState([])
+  useEffect(() =>{
+axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response =>{
+  console.log(response.data);
+  setMovies(response.data.results)
+}).catch(error=>{
+  alert('network error')
+})
+  },[])
   return (
     <div className='row'>
-        <h2>Title</h2>
+        <h2>Netflix Originals</h2>
         <div className='posters'>
-        <img className='poster' alt='poster' src='https://wallpaperaccess.com/full/1096726.jpg'   />
-        <img className='poster' alt='poster' src='https://wallpaperaccess.com/full/1766979.jpg'   />
-        <img className='poster' alt='poster' src='https://wallpaperaccess.com/full/1103413.jpg'   />
-        <img className='poster' alt='poster' src='https://wallpaperaccess.com/full/1767158.jpg'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/50/50e42bc5aef09f184dc30c43209999b2.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/a8/a8c48f0cceca41dda487ce30f1f963d0.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/99/9987dceea138d59e40dd1fb890468a9d.jpeg?h=600&r=0.5g'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/9c/9c5103b8973672d4524d76e217cf41f5.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/0f/0f0777df6bf509d8cd38b6b21f4c545f.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/26/262ed9efbf93f747371b39ca6181c339.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/8b/8ba066d7d537ac2f2c52b143609cbc45.jpeg?h=600&r=0.5'   />
-        <img className='poster' alt='poster' src='https://w.forfun.com/fetch/ea/ea489e43b2343020721a3c7d46614532.jpeg?h=600&r=0.5'   />
-        
+          {movies.map((obj)=>
+            
+     <img className='poster' alt='poster' src='https://wallpaperaccess.com/full/1096726.jpg'   />
+          )}
+   
+       
         
         </div>
     </div>
