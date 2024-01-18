386cdf2ec7c1e7c730e2a00998063d06ca984516
masood <masoodpk555@gmail.com>
2024-01-18 13:49:26 +0530

    given event-handle to react-youtube

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,13 +1,15 @@
 import React,{useEffect, useState} from 'react'
-import youtube from 'react-youtube'
+import Youtube from 'react-youtube'
 import './RowPost.css'
-import {imageUrl} from '../../constants/constants'
+import {imageUrl,API_KEY} from '../../constants/constants'
 import axios from '../../axios'
+
 
 // import axios
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [UrlId,setUrlId] = useState('')
   useEffect(() =>{
 axios.get(props.url).then(response =>{
   console.log(response.data);
@@ -16,18 +18,38 @@ axios.get(props.url).then(response =>{
   alert('network error')
 })
   },[])
+  const opts= {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handleMovie = (id)=>{
+console.log(id);
+axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+if(response.data.results.length!==0){
+  setUrlId(response.data.results[0])
+}else{
+  console.log('Not available');
+}
+})
+  }
   return (
     <div className='row'>
         <h2>{props.title}</h2>
         <div className='posters'>
           {movies.map((obj)=>
             
-     <img className={props.isSmall ? 'smallPoster' : 'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`}   />
+     <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`}   />
           )}
    
        
         
         </div>
+
+      { UrlId  && <Youtube opts={opts} videoId={UrlId.key} />}
     </div>
   )
 }
