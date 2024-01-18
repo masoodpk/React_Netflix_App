ba2153ebb9f85de217a6b0ecd9510de064dcd3dc
masood <masoodpk555@gmail.com>
2024-01-18 09:54:51 +0530

    added axios

--- src/Components/NavBar/Banner/Banner.jsx
+++ src/Components/NavBar/Banner/Banner.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Banner.css'
 
 function Banner() {
+  useEffect(()=>{
+    effect
+  },[])
   return (
     <div className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>Money Heist</h1>
             <div className='banner_buttons'>
                 <button className='button'>play</button>
                 <button className='button'>My list</button>
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+ export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = 'eb173089e8760ebd9d90cc5450ec6c04'
