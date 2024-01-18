207f42332a52c500935487b111fedc57f23dac89
masood <masoodpk555@gmail.com>
2024-01-18 10:35:51 +0530

    api called

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY} from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
 
 function Banner() {
+  const [movie, setMovie] = useState()
   useEffect(()=>{
-    effect
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0]);
+      setMovie(response.data.results[0])
+    })
   },[])
   return (
     <div className='banner'>
         <div className='content'>
-            <h1 className='title'>Money Heist</h1>
+            <h1 className='title'>{movie ? movie.title : ""} </h1>
             <div className='banner_buttons'>
                 <button className='button'>play</button>
                 <button className='button'>My list</button>
